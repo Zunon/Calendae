bbeefc206ff58b1717f3c1fb18ef661975258584
Zunon <zunon@zunon.io>
2015-12-23 17:35:20 +0400

    added hijri ui

--- src/calend/ae/Model.java
+++ src/calend/ae/Model.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import io.zunon.LibZwami;
@@ -11,40 +12,49 @@ import javafx.application.Application;
 import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Model extends Application implements Initializable {
-	
+
+	private ArrayList<Node>
+		panes = new ArrayList<>();
+	public Button
+		GregorianToHijri,
+		GregorianToZwami,
+		HijriToGregorian,
+		HijriToZwami,
+		ZwamiToGregorian,
+		ZwamiToHijri;
 	public ChoiceBox<String>
 		from,
 		to;
+	public DatePicker
+		dateGreg;
 	public GridPane
-		fromGreg,
-		fromZwami;
+		fromGregorian,
+		fromZwami,
+		root;
+	// TODO use one universal label instead of three
 	public Label
-		resultGToZ,
-		resultZToG;
-	public TextField
-		yearTextField,
-		monthTextField,
-		dayTextField,
-		zwamiTextField;
+		resultGregorian,
+		resultHijri,
+		resultZwami;
 	public static String
-		hidden="Gregorian";
-		
+		hidden = "Gregorian";
+	public TextField
+		zwamiTextField;
+	
 	public static void main(String[]args) {
 		launch(args);
-	}
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		fromZwami.setVisible(false);
 	}
 	
 	@Override
@@ -57,16 +67,28 @@ public class Model extends Application implements Initializable {
 	}
 	
 	public void onClickGToZ(Event e) {
-		int
-			year = Integer.parseInt(yearTextField.getText()),
-			month = Integer.parseInt(monthTextField.getText()),
-			dayOfMonth = Integer.parseInt(dayTextField.getText());
 		LocalDate
-			date = LocalDate.of(year, month, dayOfMonth);
+			date = dateGreg.getValue();
 		String
 			result = LibZwami.toZwami(date);
 		
-		resultGToZ.setText("The date in Zwami is: " + result);
+		resultGregorian.setText("The date in Zwami is: " + result);
+	}
+	
+	public void onClickGToH(Event e){
+		// TODO add conversion
+	}
+	
+	public void onClickHToZ(Event e) {
+		// TODO add conversion
+	}
+	
+	public void onClickHToG(Event e) {
+		// TODO add conversion
+	}
+	
+	public void onClickZToH(Event e) {
+		// TODO add conversion
 	}
 	
 	public void onClickZToG(Event e) {
@@ -79,30 +101,30 @@ public class Model extends Application implements Initializable {
 			iso = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
 		result = date.format(iso);
 		
-		resultZToG.setText(zwami + " corresponds to " + result);
+		resultZwami.setText(zwami + " corresponds to " + result);
 	}
 	
 	public void changeGridPanes(Event e) {
 		String
 			newValue = from.getValue();
-		switch(newValue) {
-		case "Gregorian":
-			fromZwami.setVisible(false);
-			fromGreg.setVisible(true);
-			to.getItems().remove("Gregorian");
-			to.getItems().add(hidden);
-			to.setValue(hidden);
-			hidden = "Gregorian";
-			break;
-		case "Zwami":
-			fromZwami.setVisible(true);
-			fromGreg.setVisible(false);
-			to.getItems().remove("Zwami");
-			to.getItems().add(hidden);
-			to.setValue(hidden);
-			hidden = "Zwami";
-			break;
-		}
+		updateStage(newValue);
+	}
+
+	private void updateStage(String newInput) {
+		panes.forEach(e -> {
+			e.setVisible(e.getId().endsWith(newInput));
+		});
+		to.getItems().remove(newInput);
+		to.getItems().add(hidden);
+		to.setValue(hidden);
+		hidden = newInput;
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		root.getChildren().stream()
+						  .filter(e -> e.getClass().getSimpleName().equals("GridPane"))
+						  .forEach(panes::add);
 	}
 
 }
